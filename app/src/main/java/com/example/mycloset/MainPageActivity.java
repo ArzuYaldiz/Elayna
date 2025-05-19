f21ff5f011c893a636051cd9b6d6c16c4175203b
Arzu Yaldız <Arzuyaldiz47@outlook.com>
2025-05-19 19:31:12 +0300

    Added MainPage

--- app/src/main/java/com/example/mycloset/MainPageActivity.java
+++ app/src/main/java/com/example/mycloset/MainPageActivity.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainPageActivity extends AppCompatActivity {
-
     private Button btnWinter, btnSummer, btnAll, btnTop, btnBottom, btnShoe;
     private RecyclerView recyclerClothes;
     private ClothesAdapter clothesAdapter;
