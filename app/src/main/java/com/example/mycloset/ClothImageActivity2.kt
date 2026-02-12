6b614d7363fa6ff38926aac9b9fd25227dfcc3d2
Arzu Yaldız <128816284+ArzuYaldiz@users.noreply.github.com>
2026-02-12 16:55:28 +0300

    Update ClothImageActivity2.kt

--- app/src/main/java/com/example/mycloset/ClothImageActivity2.kt
+++ app/src/main/java/com/example/mycloset/ClothImageActivity2.kt
@@ -72,8 +72,7 @@ class ClothImageActivity2 : ComponentActivity() {
         findViews()
 
         val retrofit = Retrofit.Builder()
-            .baseUrl("http://192.168.135.3:8080/")
-            //.baseUrl("http://10.0.2.2:8080/")
+            .baseUrl("http://10.0.2.2:8080/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
