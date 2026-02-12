0c1b5c1d61e4a958eb9925bb08676fd8d3a94318
Arzu Yaldız <128816284+ArzuYaldiz@users.noreply.github.com>
2026-02-12 16:56:39 +0300

    Update ProfilePageActivity.java

--- app/src/main/java/com/example/mycloset/ProfilePageActivity.java
+++ app/src/main/java/com/example/mycloset/ProfilePageActivity.java
@@ -54,8 +54,7 @@ public class ProfilePageActivity extends AppCompatActivity {
                 });
 
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.135.3:8080/")
-                //.baseUrl("http://10.0.2.2:8080/")
+                .baseUrl("http://10.0.2.2:8080/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
