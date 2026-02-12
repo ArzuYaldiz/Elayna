243930c63f3b5a893e79a92846bb3ded70466856
Arzu Yaldız <128816284+ArzuYaldiz@users.noreply.github.com>
2026-02-12 16:54:58 +0300

    Update AddingClothActivity.java

--- app/src/main/java/com/example/mycloset/AddingClothActivity.java
+++ app/src/main/java/com/example/mycloset/AddingClothActivity.java
@@ -146,8 +146,7 @@ public class AddingClothActivity extends AppCompatActivity {
                 .build();
 
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.135.3:8080/")
-                //.baseUrl("http://10.0.2.2:8080/")
+                .baseUrl("http://10.0.2.2:8080/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
